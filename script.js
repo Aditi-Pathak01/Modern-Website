d11104fda2383a250246be4b3f230c2616664201
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-03-23 15:28:37 +0530

    add final js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hi")
+let nav = document.querySelector("nav")
+let backTopBtn = document.querySelector(".back-top-btn")
+
+
+window.addEventListener("scroll",()=>{
+ nav.classList.add("scroll");
+if (scrollY > 250){
+ backTopBtn.style.display = "block"
+} else {
+  backTopBtn.style.display = "none"
+}})
+
+backTopBtn.addEventListener("click",()=>{
+  document.documentElement.scrollTop = 0;
+})
