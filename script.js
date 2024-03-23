0d0cb695829d0f6953a200f61b14afe632307a1a
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-03-23 19:25:55 +0530

    add final js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-let nav = document.querySelector("nav")
-let backTopBtn = document.querySelector(".back-top-btn")
+let nav = document.querySelector("nav");
+let backTopBtn = document.querySelector(".back-top-btn");
 
+window.addEventListener("scroll", () => {
+  if (scrollY > 0) {
+    nav.classList.add("scroll");
+  } else {
+    nav.classList.remove("scroll");
+  }
+  if (scrollY > 250) {
+    backTopBtn.style.display = "block";
+  } else {
+    backTopBtn.style.display = "none";
+  }
+});
 
-window.addEventListener("scroll",()=>{
-if (scrollY > 250){
- backTopBtn.style.display = "block"
- nav.classList.add("scroll");
-} else {
-  backTopBtn.style.display = "none"
-  nav.classList.remove("scroll");
-}})
-
-backTopBtn.addEventListener("click",()=>{
+backTopBtn.addEventListener("click", () => {
   document.documentElement.scrollTop = 0;
-})
+});
